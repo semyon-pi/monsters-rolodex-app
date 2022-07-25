8eeaa7ed8531ae6377d033b5daf561bbad6d1a10
semyon <pinhassons@gmail.com>
2022-07-26 00:58:32 +0300

    class components practice

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Card from '../card/card.component'
+import './card-list.styles.css'
 
 class CardList extends Component {
   render() {
@@ -6,17 +8,10 @@ class CardList extends Component {
     console.log('render from CardList')
 
     return (
-      <div>
-        {monsters.map((monster) => (
-          <div className='card-container' key={monster.id}>
-            <img
-              alt={`monster ${monster.name}`}
-              src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
-            />
-            <h2>{monster.name}</h2>
-            <p>{monster.email}</p>
-          </div>
-        ))}
+      <div className='card-list'>
+        {monsters.map((monster) => {
+          return <Card monster={monster} />
+        })}
       </div>
     )
   }
